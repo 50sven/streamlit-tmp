7f25b36f27fe3b9fce3911ed5f382fcdd276a0ba
Sven Müller <39131261+50sven@users.noreply.github.com>
2023-03-12 15:07:59 +0100

    Update streamlit_app.py
    add insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+fruit_add = streamlit.text_input('What fruit would you like to add?', None)
+streamlit.write('Thanks for adding ', fruit_choice)
+if fruit_add is not None:
+  my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+  
